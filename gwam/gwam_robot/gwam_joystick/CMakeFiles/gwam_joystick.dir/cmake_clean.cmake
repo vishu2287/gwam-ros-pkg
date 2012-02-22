cfc121f6401696c803e7cb7996a6ea4cee5350cd
rguzman <rguzman@robotnik.es>
2012-02-22 18:52:57 +0000

    added gwam_joystick

---
+++ gwam/gwam_robot/gwam_joystick/CMakeFiles/gwam_joystick.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/gwam_joystick.dir/src/gwam_joystick.o"
+  "bin/gwam_joystick.pdb"
+  "bin/gwam_joystick"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/gwam_joystick.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
