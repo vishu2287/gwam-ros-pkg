c32f943ff51e79bfdf08d9d50bdbf564a18fa4a5
KyleMaroney <kylemaroney@gmail.com>
2012-02-24 16:12:09 +0000

    Adding more functionality to wam_joystick_teleop for gwam_homing and holding joint positions.

--- wam/wam_robot/wam_joystick_teleop/src/teleop_joystick.cpp
+++ wam/wam_robot/wam_joystick_teleop/src/teleop_joystick.cpp
@@ -6,6 +6,8 @@
 #include "wam_msgs/BHandSpreadVel.h"
 #include "wam_msgs/CartMove.h"
 #include "wam_msgs/RTCart.h"
+#include "wam_msgs/HoldJointPosition.h"
+#include "std_srvs/Empty.h"
 #include <sensor_msgs/Joy.h>
 #include <geometry_msgs/PoseStamped.h>
 
@@ -16,11 +18,11 @@ class WamTeleop
 public:
   ros::NodeHandle nh_;
   bool deadman, first_rt;
-  bool grsp_publish, sprd_publish, cart_publish;
-  bool quat_publish, pose_pubbed;
+  bool grsp_publish, sprd_publish, cart_publish, home_publish, hold_publish;
+  bool quat_publish, pose_pubbed, home_last, hold_last, home_st, hold_st, holding;
   int deadman_button, gpr_open_btn, gpr_close_btn;
   int sprd_open_btn, sprd_close_btn, bh_cmd_last;
-  int quat_btn;
+  int quat_btn, home_btn, hold_btn;
   int axis_dx, axis_dy, axis_dz;
   double bh_max_gsp_vel, bh_max_sprd_vel;
   double bh_gsp_vel, bh_sprd_vel;
@@ -33,11 +35,15 @@ public:
   wam_msgs::BHandSpreadVel sprd_vel_srv;
   wam_msgs::CartMove cart_move_srv;
   wam_msgs::RTCart rt_position;
+  wam_msgs::HoldJointPosition hold_jpos_srv;
+  std_srvs::Empty go_home_srv;
   ros::Subscriber joy_sub;
   ros::Subscriber pose_sub;
   ros::ServiceClient grasp_vel;
   ros::ServiceClient spread_vel;
   ros::ServiceClient cart_move;
+  ros::ServiceClient go_home;
+  ros::ServiceClient hold_jpos;
   ros::Publisher rt_cart_pub;
 
   WamTeleop() :
@@ -53,6 +59,8 @@ public:
     nh_.param("spread_open_button", sprd_open_btn, 13);
     nh_.param("spread_close_button", sprd_close_btn, 15);
     nh_.param("rotation_control_button", quat_btn, 10);
+    nh_.param("go_home_button", home_btn, 0);
+    nh_.param("hold_position_button", hold_btn, 3);
     nh_.param("grasp_max_velocity", bh_max_gsp_vel, 1.0);
     nh_.param("spread_max_velocity", bh_max_sprd_vel, 1.0);
     nh_.param("cartesian_x_delta", wam_max_dx, 0.008);
@@ -66,12 +74,14 @@ public:
 
     rt_position.position.resize(3);
 
-    joy_sub = nh_.subscribe < sensor_msgs::Joy > ("joy", 1, &WamTeleop::joyCallback, this);
-    pose_sub = nh_.subscribe < geometry_msgs::PoseStamped > ("wam/pose", 1, &WamTeleop::poseCallback, this);
-    grasp_vel = nh_.serviceClient < wam_msgs::BHandGraspVel > ("bhand/grasp_vel");
-    spread_vel = nh_.serviceClient < wam_msgs::BHandSpreadVel > ("bhand/spread_vel");
-    cart_move = nh_.serviceClient < wam_msgs::CartMove > ("wam/cartesian_pos");
-    rt_cart_pub = nh_.advertise < wam_msgs::RTCart > ("wam/rtposition", 1);
+    joy_sub = nh_.subscribe<sensor_msgs::Joy>("joy", 1, &WamTeleop::joyCallback, this);
+    pose_sub = nh_.subscribe<geometry_msgs::PoseStamped>("wam/pose", 1, &WamTeleop::poseCallback, this);
+    grasp_vel = nh_.serviceClient<wam_msgs::BHandGraspVel>("bhand/grasp_vel");
+    spread_vel = nh_.serviceClient<wam_msgs::BHandSpreadVel>("bhand/spread_vel");
+    cart_move = nh_.serviceClient<wam_msgs::CartMove>("wam/cartesian_pos");
+    rt_cart_pub = nh_.advertise<wam_msgs::RTCart>("wam/rtposition", 1);
+    go_home = nh_.serviceClient<std_srvs::Empty>("wam/go_home");
+    hold_jpos = nh_.serviceClient<wam_msgs::HoldJointPosition>("wam/hold_jpos");
 
     cart_move_srv.request.coordinates.resize(3);
   }
@@ -87,10 +97,8 @@ public:
 
 void WamTeleop::joyCallback(const sensor_msgs::Joy::ConstPtr& joy_msg)
 {
-  grsp_publish = false;
-  sprd_publish = false;
-  cart_publish = false;
-  quat_publish = false;
+  grsp_publish = sprd_publish = cart_publish = quat_publish = home_publish = hold_publish = home_last = hold_last =
+      home_st = hold_st = holding = false;
 
   if (!joy_msg->buttons[deadman_button] || !pose_pubbed) // this returns with no deadman pressed or without first pose published yet.
     return;
@@ -144,11 +152,45 @@ void WamTeleop::joyCallback(const sensor_msgs::Joy::ConstPtr& joy_msg)
   else
     req_cdz = 0.0;
 
+  //For go_home command
+  if (joy_msg->buttons[gpr_open_btn])
+  {
+    bh_gsp_vel = -bh_max_gsp_vel;
+    grsp_publish = true;
+  }
+  else if (joy_msg->buttons[gpr_close_btn])
+  {
+    bh_gsp_vel = bh_max_gsp_vel;
+    grsp_publish = true;
+  }
+
+  home_st = (joy_msg->buttons[home_btn]);
+  if (home_st && !home_last)
+  {
+    home_publish = true;
+  }
+  else
+  {
+    home_publish = false;
+  }
+  home_last = home_publish;
+  //for hold joint position command
+  hold_st = (joy_msg->buttons[hold_btn]);
+  if (hold_st && !hold_last)
+  {
+    hold_publish = true;
+  }
+  else
+  {
+    hold_publish = false;
+  }
+
 }
 
 void WamTeleop::poseCallback(const geometry_msgs::PoseStamped::ConstPtr& pose_msg)
 {
-  if(pose_pubbed == false){
+  if (pose_pubbed == false)
+  {
     cart_move_srv.request.coordinates[0] = pose_msg->pose.position.x;
     cart_move_srv.request.coordinates[1] = pose_msg->pose.position.y;
     cart_move_srv.request.coordinates[2] = pose_msg->pose.position.z;
@@ -165,6 +207,7 @@ void WamTeleop::poseCallback(const geometry_msgs::PoseStamped::ConstPtr& pose_ms
 
 void WamTeleop::send_commands()
 {
+
   if (grsp_publish && bh_cmd_last == 0 && !cart_publish)
   {
     gsp_vel_srv.request.velocity = bh_gsp_vel;
@@ -193,30 +236,33 @@ void WamTeleop::send_commands()
   }
   if (cart_publish && !grsp_publish && !sprd_publish && pose_pubbed)
   {
-    //std::cerr << "cart currents: " << cart_cur[0] << " " <<  cart_cur[1] << " " << cart_cur[2] << std::endl;
-    //std::cerr << "requesting coordinates: " << cart_move_srv.request.coordinates[0] << " " << cart_move_srv.request.coordinates[1] << " " << cart_move_srv.request.coordinates[2] << std::endl;
-/*    double diff = sqrt(
-        (cart_move_srv.request.coordinates[0] - cart_cur[0]) * (cart_move_srv.request.coordinates[0] - cart_cur[0])
-            + (cart_move_srv.request.coordinates[1] - cart_cur[1]) * (cart_move_srv.request.coordinates[1] - cart_cur[1])
-            + (cart_move_srv.request.coordinates[2] - cart_cur[2]) * (cart_move_srv.request.coordinates[2] - cart_cur[2]));
-    std::cerr << "diff = " << diff << std::endl;
-    if (diff < 0.005)
-    {*/
-      rt_position.position[0] = cart_cur[0] + req_cdx;
-      rt_position.position[1] = cart_cur[1] + req_cdy;
-      rt_position.position[2] = cart_cur[2] + req_cdz;
-      //cart_move_srv.request.coordinates[0] = cart_cur[0] + req_cdx;
-      //cart_move_srv.request.coordinates[1] = cart_cur[1] + req_cdy;
-      //cart_move_srv.request.coordinates[2] = cart_cur[2] + req_cdz;
-      //cart_move.call(cart_move_srv);
-      std::cerr << "published position = " << rt_position.position[2] << std::endl;
-      rt_cart_pub.publish(rt_position);
-      cart_last[0] = rt_position.position[0];
-      cart_last[1] = rt_position.position[1];
-      cart_last[2] = rt_position.position[2];
-    //}
+    rt_position.position[0] = cart_cur[0] + req_cdx;
+    rt_position.position[1] = cart_cur[1] + req_cdy;
+    rt_position.position[2] = cart_cur[2] + req_cdz;
+    rt_cart_pub.publish(rt_position);
+    cart_last[0] = rt_position.position[0];
+    cart_last[1] = rt_position.position[1];
+    cart_last[2] = rt_position.position[2];
     pose_pubbed = false;
   }
+
+  if (hold_publish && !cart_publish && !grsp_publish && !sprd_publish && !home_publish)
+  {
+    if (!holding)
+    {
+      hold_jpos_srv.request.hold = true;
+      holding = true;
+    }
+    else
+    {
+      hold_jpos_srv.request.hold = false;
+      holding = false;
+    }
+    hold_jpos.call(hold_jpos_srv);
+  }
+
+  if(home_publish && !hold_publish && !cart_publish && !grsp_publish && !sprd_publish)
+    go_home.call(go_home_srv);
 }
 
 int main(int argc, char** argv)
